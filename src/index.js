b93f23c694dcc2c900a0a873b7687f3b9e718f98
Isabela G.C <isabelagrisalescastrillon@gmail.com>
2025-03-23 07:50:44 -0500

    Se funcionan cambios y se arregla modal de formulario registro

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
 import App from './App'; // Tu componente principal
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
